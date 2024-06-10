5df2f18cb04f5f7a4b5315d5602812553973d754
Javi Rodriguez <3051496+lifeBalance@users.noreply.github.com>
2024-06-10 18:27:57 +0300

    add sauce to the form

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -1,144 +1,170 @@
-<script setup></script>
+<script setup>
+const selectOptions = [
+  { value: '', label: 'Please Select an Option' },
+  { value: 'booking', label: 'Booking' },
+  { value: 'courses', label: 'Courses' },
+  { value: 'anything', label: 'Anything' },
+]
+</script>
 
 <template>
-  <section class="contact">
-    <div class="text-box">
-      <h2>Want to send us a message?</h2>
+  <section class="contact-form">
+    <div class="form-box">
+      <div class="text-box">
+        <h2>Want to send us a message?</h2>
 
-      <p class="cta-text">
-        If you have some <strong>question</strong> about a course, an
-        <strong>incidence</strong> regarding your booking, or just at
-        <strong>suggestion</strong>, please feel free to send us a message. We
-        will be happy to help you.
-      </p>
+        <p class="cta-text">
+          If you have some <strong>question</strong> about a course, an
+          <strong>incidence</strong> regarding your booking, or just at
+          <strong>suggestion</strong>, please feel free to send us a message. We
+          will be happy to help you.
+        </p>
+      </div>
+
+      <form action="#" name="sign-up">
+        <div class="full-name">
+          <label for="full-name">Full name</label>
+          <input
+            id="full-name"
+            type="text"
+            placeholder="John Doe"
+            name="full-name"
+            required
+            autocomplete="off"
+          />
+        </div>
+
+        <div class="email">
+          <label for="email">Email</label>
+          <input
+            id="email"
+            type="email"
+            placeholder="me@example.com"
+            name="email"
+            required
+            autocomplete="off"
+          />
+        </div>
+
+        <div class="select">
+          <label for="select-where">What is your message about?</label>
+
+          <CustomSelect
+            label="What is your message about?"
+            :options="selectOptions"
+          />
+        </div>
+
+        <div class="message">
+          <label for="email">Your Message</label>
+          <textarea
+            id="text"
+            placeholder="Your message..."
+            name="message"
+            required
+            autocomplete="off"
+          ></textarea>
+        </div>
+
+        <button class="btn btn--form">Submit</button>
+      </form>
     </div>
+    <!-- .form-box -->
 
-    <form action="#" name="sign-up">
-      <div class="full-name">
-        <label for="full-name">Full name</label>
-        <input
-          id="full-name"
-          type="text"
-          placeholder="John Doe"
-          name="full-name"
-          required
-          autocomplete="off"
-        />
-      </div>
-
-      <div class="email">
-        <label for="email">Email</label>
-        <input
-          id="email"
-          type="email"
-          placeholder="me@example.com"
-          name="email"
-          required
-          autocomplete="off"
-        />
-      </div>
-
-      <div class="select">
-        <label for="select-where">What is your message about?</label>
-        <select id="select-where" name="select-where" required>
-          <option value="">Please select an option:</option>
-          <option value="booking">Incidence during Booking</option>
-          <option value="course">Question about a course</option>
-          <option value="anything">Anything else</option>
-        </select>
-      </div>
-
-      <div class="message">
-        <label for="email">Your Message</label>
-        <textarea
-          id="text"
-          placeholder="Your message..."
-          name="message"
-          required
-          autocomplete="off"
-        ></textarea>
-      </div>
-
-      <button class="btn btn--form">Submit</button>
-    </form>
+    <div class="img-box"></div>
   </section>
 </template>
 
 <style lang="scss" scoped>
-section {
-  background-color: #23262d;
-  padding: 1rem;
-  border-radius: 7px;
-  box-shadow: inset 0 0 0 1px rgba(255, 255, 255, 0.1);
+.contact-form {
   display: flex;
-  flex-direction: column;
-  gap: 2rem;
-  max-width: 90%;
+  // grid-template-columns: 1fr;
   margin: 0 auto;
-  padding: 2rem;
 
-  p {
-    color: rgb(var(--color-text-1), 0.8);
-    line-height: 1.4;
-  }
+  & .form-box {
+    background-color: #23262d;
+    padding: 1rem;
+    border-radius: 7px;
+    border-top-right-radius: 0;
+    border-bottom-right-radius: 0;
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    max-width: 90%;
+    min-width: 50%;
+    margin: 0 auto;
+    padding: 2rem;
 
-  h2 {
-    color: rgb(var(--color-text-1));
-    margin-bottom: 1rem;
-  }
+    p {
+      color: rgb(var(--color-text-1), 0.8);
+      line-height: 1.4;
+    }
 
-  form {
-    display: grid;
-    grid-template-columns: 1fr;
-    row-gap: 2rem;
-    column-gap: 1rem;
-
-    label {
+    h2 {
       color: rgb(var(--color-text-1));
-      display: block;
-      font-weight: 500;
-      margin-bottom: 0.8rem;
+      margin-bottom: 1rem;
     }
 
-    & *:focus {
-      outline: none;
-      box-shadow: 0 0 0 0.2rem rgba(var(--color-accent-1), 0.9);
-    }
+    form {
+      display: grid;
+      grid-template-columns: 1fr;
+      row-gap: 2rem;
+      column-gap: 1rem;
 
-    input,
-    textarea,
-    select {
-      padding: 0.5rem;
-      font-size: 1.2rem;
-      font-family: inherit;
-      color: rgb(var(--color-text-2));
-      border: 1px solid rgba(var(--color-text-1), 0.2);
-      background-color: rgba(var(--color-bg), 0.9);
-      border-radius: 7px;
-      box-shadow: 0 1px 2px rgba(var(--color-bg), 0.1);
-      width: 100%;
-    }
+      label {
+        color: rgb(var(--color-text-1));
+        display: block;
+        font-weight: 500;
+        margin-bottom: 0.8rem;
+      }
 
-    button {
-      all: unset;
-      cursor: pointer;
-      padding: 1rem 2rem;
-      color: rgb(var(--color-accent-1));
-      border: 1px solid rgb(var(--color-accent-1));
-      border-radius: 5px;
-      text-align: center;
-      display: inline-block;
-      text-decoration: none;
-      font-size: clamp(1.2rem, 4vw, 2rem);
-      font-weight: 500;
-      transition: all 0.3s ease-in-out;
+      & *:focus {
+        outline: none;
+        box-shadow: 0 0 0 0.2rem rgba(var(--color-accent-1), 0.9);
+      }
 
-      &:hover,
-      &:active {
-        background-color: rgba(var(--color-accent-1), 0.2);
-        scale: 1.01;
+      .select {
+        grid-column: 1 / -1;
+      }
+
+      input,
+      textarea {
+        padding: 0.5rem;
+        font-size: 1.2rem;
+        font-family: inherit;
+        color: rgb(var(--color-text-2));
+        border: 1px solid rgba(var(--color-text-1), 0.2);
+        background-color: rgba(var(--color-bg), 0.9);
+        border-radius: 7px;
+        box-shadow: 0 1px 2px rgba(var(--color-bg), 0.1);
+        width: 100%;
+      }
+
+      button {
+        all: unset;
+        cursor: pointer;
+        padding: 1rem 2rem;
+        color: rgb(var(--color-accent-1));
+        border: 1px solid rgb(var(--color-accent-1));
+        border-radius: 5px;
+        text-align: center;
+        display: inline-block;
+        text-decoration: none;
+        font-size: clamp(1.2rem, 4vw, 2rem);
+        font-weight: 500;
+        transition: all 0.3s ease-in-out;
+
+        &:hover,
+        &:active {
+          background-color: rgba(var(--color-accent-1), 0.2);
+          scale: 1.01;
+        }
       }
     }
+  } // & .form-box
+
+  & .img-box {
+    display: none;
   }
 }
 
@@ -153,30 +179,46 @@ section {
 }
 
 @media (min-width: 768px) {
-  section {
-    form {
-      grid-template-columns: 1fr 1fr;
-    }
+  .contact-form {
+    grid-template-columns: 1fr 1fr;
 
-    .full-name,
-    .email {
-      grid-row: 1;
-    }
+    & .form-box {
+      form {
+        grid-template-columns: 1fr 1fr;
+      }
 
-    .select {
-      grid-column: 1;
-      /* grid-row: 1; */
-    }
-    .message {
-      grid-row: 3;
-      grid-column: span 2;
-    }
+      .full-name,
+      .email {
+        grid-row: 1;
+      }
 
-    .btn {
-      grid-column: 1 / -1;
-      /* grid-row: -1; */
-    }
-  }
+      .select {
+        grid-column: 1 / -1;
+        /* grid-row: 1; */
+      }
+      .message {
+        grid-row: 3;
+        grid-column: span 2;
+      }
+
+      .btn {
+        grid-column: 1 / -1;
+        /* grid-row: -1; */
+      }
+    } // & .form-box
+
+    & .img-box {
+      grid-column: 2;
+      display: block;
+      border-radius: 0.5rem;
+      border-top-left-radius: 0;
+      border-bottom-left-radius: 0;
+      min-width: 50%;
+      background-image: url('~/assets/images/armory4.jpeg');
+      background-size: cover;
+      background-position: center;
+    } // & .img-box
+  } // .contact-form
 }
 
 @media (min-width: 992px) {
---
+++ components/CustomSelect.vue
@@ -0,0 +1,80 @@
+<script setup>
+const { options, label } = defineProps(['label', 'options'])
+</script>
+
+<template>
+  <div class="custom-select">
+    <select>
+      <option
+        v-for="option in options"
+        :value="option.value"
+        :key="option.value"
+      >
+        {{ option.label }}
+      </option>
+    </select>
+    <span class="custom-arrow"></span>
+  </div>
+</template>
+
+<style>
+.custom-select {
+  position: relative;
+
+  select {
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+
+    padding: 0.5rem;
+    padding-right: 2rem;
+    font-size: 1.2rem;
+    font-family: inherit;
+    color: rgb(var(--color-text-2));
+    border: 1px solid rgba(var(--color-text-1), 0.2);
+    background-color: rgba(var(--color-bg), 0.9);
+    border-radius: 7px;
+    box-shadow: 0 1px 2px rgba(var(--color-bg), 0.1);
+    width: 100%;
+    cursor: pointer;
+  }
+
+  .custom-arrow {
+    position: absolute;
+    display: block;
+    top: 0;
+    right: 0;
+    height: 100%;
+    width: 2rem;
+    border-top-right-radius: 7px;
+    border-bottom-right-radius: 7px;
+    background-color: rgb(var(--color-text-2), 0.7);
+    pointer-events: none;
+  }
+
+  .custom-arrow::before,
+  .custom-arrow::after {
+    --size: .5rem;
+    content: '';
+    position: absolute;
+    width: 0;
+    height: 0;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+  }
+
+  .custom-arrow::before {
+    border-left: var(--size) solid transparent;
+    border-right: var(--size) solid transparent;
+    border-bottom: var(--size) solid rgb(var(--color-bg));
+    top: 30%;
+  }
+  .custom-arrow::after {
+    border-left: var(--size) solid transparent;
+    border-right: var(--size) solid transparent;
+    border-top: var(--size) solid rgb(var(--color-bg));
+    top: 60%;
+  }
+}
+</style>
