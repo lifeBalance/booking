5ce94a04aa82053ac1a597e674a1a770b708d95d
Javi Rodriguez <3051496+lifeBalance@users.noreply.github.com>
2024-06-16 10:42:03 +0300

    finish useTheme composable

--- composables/useTheme.js
+++ composables/useTheme.js
@@ -1,35 +1,41 @@
 export default function useTheme() {
-  // We'll use this ref to keep track of the checkbox state (theme switcher)
-  const isDark = ref(false)
+  // We'll store the isDark flag (boolean) as GLOBAL state, so that we can 
+  // use it anywhere in our app (mainly for styling purposes).
+  const isDark = useState('isDarkG', () => {
+    if (typeof window !== 'undefined') {
+      // 1. Check if the user has a theme preference set in localStorage
+      const localStorageTheme = localStorage.getItem('theme')
+      // 2. Check if the user has DARK as a system's theme preference
+      const theme = window.matchMedia('(prefers-color-scheme: dark)')
 
-  // 1. Check if the user has a theme preference set in localStorage
-  const localStorageTheme = localStorage.getItem('theme')
-  // 2. Check if the user has a  system's theme preference
-  const theme = window.matchMedia('(prefers-color-scheme: dark)')
-
-  // Set the theme based on the user's preference, giving priority to
-  // the setting stored in local storage over the system's preference.
-  if (localStorageTheme) {
-    document.documentElement.setAttribute('data-theme', localStorageTheme)
-    isDark.value = localStorageTheme === 'dark'
-  } else if (theme.matches) {
-    document.documentElement.setAttribute('data-theme', 'dark')
-    isDark.value = true
-  } else {
-    document.documentElement.setAttribute('data-theme', 'light')
-    isDark.value = false
-  }
+      // Set the theme based on the user's preference, giving priority to
+      // the setting stored in local storage over the system's preference.
+      if (localStorageTheme) {
+        document.documentElement.setAttribute('data-theme', localStorageTheme)
+        return localStorageTheme === 'dark'
+      } else if (theme.matches) {
+        document.documentElement.setAttribute('data-theme', 'dark')
+        return true
+      } else {
+        document.documentElement.setAttribute('data-theme', 'light')
+        return false
+      }
+    }
+  })
 
   const toggleTheme = () => {
     isDark.value = !isDark.value
-    console.log('toggleTheme', isDark.value) // testing
+    // console.log('toggleTheme', isDark.value) // testing
 
-    localStorage.setItem('theme', isDark.value ? 'dark' : 'light')
-    document.documentElement.setAttribute(
-      'data-theme',
-      isDark.value ? 'dark' : 'light'
-    )
+    // We need this check because the window object is not available during SSR
+    if (typeof window !== 'undefined') {
+      localStorage.setItem('theme', isDark.value ? 'dark' : 'light')
+      document.documentElement.setAttribute(
+        'data-theme',
+        isDark.value ? 'dark' : 'light'
+      )
+    }
   }
 
-  return { isDark, toggleTheme }
+  return { isDark, toggleTheme}
 }
